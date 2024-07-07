2c7e6863c5d85ce717cb326d52148a1461290068
Victor Mendes <victormendes04012004@gmail.com>
2024-07-07 17:53:48 -0300

    emit billing order_created

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { OrdersRepository } from './orders.repository';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
+  private readonly logger = new Logger(OrdersService.name);
+  async createOrder(request: CreateOrderRequest) {
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+        }),
+      );
+      await session.commitTransaction();
+      this.logger.log('Order...', order);
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
+  }
+  async getOrders() {
+    return this.ordersRepository.find({});
   }
 }
